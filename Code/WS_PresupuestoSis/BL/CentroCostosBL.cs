0302a64d643e455834809fca07394131b7c5890c
INFORMATICA\gespinoza <heiner8907@gmail.com>
2017-06-23 21:14:20 -0600

    initial setup

---
+++ Code/WS_PresupuestoSis/BL/CentroCostosBL.cs
@@ -0,0 +1,37 @@
+﻿using LD;
+using System.Collections.Generic;
+using ModelMap;
+using System.Linq;
+
+namespace BL
+{
+    public class CentroCostosBL:BaseModel
+    {
+
+        public CentroCostosLD _CentroCostos
+        {
+            get
+            {
+                return new CentroCostosLD();
+            }
+        }
+
+
+        public List<CentroCostosMap> getCentroCostos()
+        {
+            var ListaCentroCostos = new List<CentroCostosMap>();
+            foreach (var item in _CentroCostos.getCentrosCosto().ToList())
+            {
+                ListaCentroCostos.Add
+                    (
+                        new CentroCostosMap
+                        {
+                            Descripcion = item.DESCRIPCION
+                        }
+                    );
+            }
+            return ListaCentroCostos;
+        }
+
+    }
+}
---
+++ Code/WS_PresupuestoSis/WS_PresupuestoSis/CatalogosProcces.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ModelMap;
+using BL;
+
+namespace WS_PresupuestoSis
+{
+    public class CatalogosProcces
+    {
+        public CentroCostosBL centroCostosBL
+        {
+            get
+            {
+                return new CentroCostosBL();
+            }
+        }
+        public List<CentroCostosMap> getCentrosCosto()
+        {
+             return centroCostosBL.getCentroCostos();
+        }
+
+    }
+}
