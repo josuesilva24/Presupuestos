b8783316c718ec43d5afaef336dc1fb5675c3dab
Josue <josuesiilva24@gmail.com>
2017-07-19 21:47:18 -0600

    Add ingresos

--- Code/WS_PresupuestoSis/BL/IngresosBL.cs
+++ Code/WS_PresupuestoSis/BL/IngresosBL.cs
@@ -11,16 +11,14 @@ namespace BL
         readonly Lazy<IngresosLD> _ingresosLD = new Lazy<IngresosLD>(() => new IngresosLD());
         IngresosLD IngresosLD { get { return _ingresosLD.Value; } }
 
-        public bool AddIngresos(int moneda, string descripcion, int ano, decimal enero,
+        public int AddIngresosWithReturnId(int moneda, string descripcion, int ano, decimal enero,
                                 decimal febrero, decimal marzo, decimal abril, decimal mayo,
                                 decimal junio, decimal julio, decimal agosto, decimal septiembre,
                                 decimal octubre, decimal noviembre, decimal diciembre, bool lineal,
                                 bool puntual, decimal monto, int idCentroCostos, int iCuentaContable)
         {
-           IngresosLD.AddIngresos(moneda, descripcion, ano, enero, febrero, marzo, abril, mayo,
-                                 junio, julio, agosto, septiembre, octubre, noviembre, diciembre,
-                                 lineal, puntual, monto, idCentroCostos, iCuentaContable);
-            return true;
+            return IngresosLD.AddIngresosWithReturnId(moneda, descripcion, ano, enero, febrero, marzo, abril, mayo, junio, julio, agosto,
+                                                      septiembre, octubre, noviembre, diciembre, lineal, puntual, monto, idCentroCostos, iCuentaContable);
         }
 
         public List<IngresosMap> GetAllIngresos()
